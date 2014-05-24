f7e1bca371dce3c2152a519012b71ac7acd92631
Atsuo Ishimoto <ishimoto@gembook.org>
2014-05-24 16:39:53 +0900

    rss settings.

--- publishconf.py
+++ publishconf.py
@@ -13,8 +13,10 @@ from pelicanconf import *
 SITEURL = 'http://www.python.jp'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = 'all.atom.xml'
+FEED_ALL_RSS = 'channews.rdf'
+
+
 
 DELETE_OUTPUT_DIRECTORY = True
 
